7b0670de61c9598724a5a70bd10286f266c13fcd
Jhon <alexkross755@gmail.com>
2023-03-22 01:53:10 +0300

    -A4

--- tic-tac.py
+++ tic-tac.py
@@ -1,8 +1,7 @@
 
 sq = [[' '] * 3 for q in range(3)]
 
-
-def grate():
+def grate():                        # Создаём решётку
     coord_y = '  | 0 | 1 | 2 |'
     print(coord_y)
     print('_' * len(coord_y))
@@ -22,27 +21,25 @@ while True:
     else:
         n = 'X'
 
-
     h, v = input(f"Введите координаты '{n}' через пробел: ").split()
     if not(h.isdigit()) or not(v.isdigit()):
         print('Введите цифры')
         count -= 1
         continue
+
     h, v = int(h), int(v)
     if h < 0 or h > 2 or v < 0 or v >2:
         print('Координаты в не диапазона')
         count -= 1
         continue
 
-    if sq[h][v] == 'O' or sq[h][v] == 'X':
+    if sq[h][v] == 'O' or sq[h][v] == 'X':   #
         print('Эта клетка занята, выберите другую.')
         count -= 1
         continue
     else:
         sq[h][v] = n
-
-
-
+# Проверка выйгрышных вариантов:
     if sq[0][0] == sq[0][1] == sq[0][2] == n or sq[1][0] == sq[1][1] == sq[1][2] == n \
             or sq[2][0] == sq[2][1] == sq[2][2] == n or sq[0][0] == sq[1][0] == sq[2][0] == n \
             or sq[0][1] == sq[1][1] == sq[2][1] == n or sq[0][2] == sq[1][2] == sq[2][2] == n \
@@ -51,14 +48,10 @@ while True:
         print('Winner :', n)
         break
 
-
-
     if count == 9:
         grate()
         print('Ничья!')
         break
-
-
 
     grate()
 
